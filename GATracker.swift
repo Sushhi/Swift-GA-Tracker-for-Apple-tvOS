25959c34ca0b2a77edb22ae825960e30635c95b6
Emilien ROUSSEL <emilienroussel9@gmail.com>
2017-05-15 09:37:24 +0400

    Update Swift 3

--- GATracker.swift
+++ GATracker.swift
@@ -12,14 +12,14 @@ private var _analyticsTracker: GATracker!
 
 class GATracker {
     /*
-        Define properties
-        @tid = Google Analytics property id
-        @cid = Google Analytics client id
-        @appName = Application Name 
-        @appVersion = Application Version
-        @MPVersion = Measurement Protocol version
-        @ua = User Agent string
-    */
+     Define properties
+     @tid = Google Analytics property id
+     @cid = Google Analytics client id
+     @appName = Application Name
+     @appVersion = Application Version
+     @MPVersion = Measurement Protocol version
+     @ua = User Agent string
+     */
     private var tid : String
     var cid: String
     var appName : String
@@ -30,12 +30,9 @@ class GATracker {
     
     //Set up singleton object for the tracker
     class func setup(tid: String) -> GATracker {
-        struct Static {
-            static var onceToken: dispatch_once_t = 0
-        }
-        dispatch_once(&Static.onceToken) {
+        let _: () = {
             _analyticsTracker = GATracker(tid: tid)
-        }
+        }()
         return _analyticsTracker
     }
     
@@ -50,30 +47,30 @@ class GATracker {
     
     init(tid: String) {
         /*
-            Initialize Tracker with Property Id
-            Set up all attributes
-        */
+         Initialize Tracker with Property Id
+         Set up all attributes
+         */
         #if DEBUG
             print("Google Analytics Tracker Initialized")
         #endif
         
         self.tid = tid
-        self.appName = NSBundle.mainBundle().infoDictionary!["CFBundleName"] as! String
-        let nsObject: AnyObject? = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"]
+        self.appName = Bundle.main.infoDictionary!["CFBundleName"] as! String
+        let nsObject: AnyObject? = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as AnyObject?
         self.appVersion = nsObject as! String
         self.ua = "Mozilla/5.0 (Apple TV; CPU iPhone OS 9_0 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13T534YI"
         self.MPVersion = "1"
-        let defaults = NSUserDefaults.standardUserDefaults()
-        if let cid = defaults.stringForKey("cid") {
+        let defaults = UserDefaults.standard
+        if let cid = defaults.string(forKey: "cid") {
             self.cid = cid
         }
         else {
-            self.cid = NSUUID().UUIDString
-            defaults.setObject(self.cid, forKey: "cid")
+            self.cid = NSUUID().uuidString
+            defaults.set(self.cid, forKey: "cid")
         }
         
-        let language = NSLocale.preferredLanguages().first
-        if language?.characters.count > 0 {
+        let language = NSLocale.preferredLanguages.first
+        if (language?.characters.count)! > 0 {
             self.ul = language!
         } else {
             self.ul = "(not set)"
@@ -82,38 +79,38 @@ class GATracker {
     
     func send(type: String, params: Dictionary<String, String>) {
         /*
-            Generic hit sender to Measurement Protocol
-            Consists out of hit type and a dictionary of other parameters
-        */
+         Generic hit sender to Measurement Protocol
+         Consists out of hit type and a dictionary of other parameters
+         */
         let endpoint = "https://www.google-analytics.com/collect?"
         var parameters = "v=" + self.MPVersion + "&an=" + self.appName + "&tid=" + self.tid + "&av=" + self.appVersion + "&cid=" + self.cid + "&t=" + type + "&ua=" + self.ua + "&ul=" + self.ul
-
+        
         for (key, value) in params {
             parameters += "&" + key + "=" + value
         }
         
         //Encoding all the parameters
-        if let paramEndcode = parameters.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLPathAllowedCharacterSet()){
+        if let paramEndcode = parameters.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed){
             let urlString = endpoint + paramEndcode;
-            let url = NSURL(string: urlString);
+            let url = URL(string: urlString);
             
             #if DEBUG
                 print(urlString)
             #endif
             
-            let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) -> Void in
-                if let httpReponse = response as? NSHTTPURLResponse {
+            let task = URLSession.shared.dataTask(with: url!) { (data, response, error) -> Void in
+                if let httpReponse = response as? HTTPURLResponse {
                     let statusCode = httpReponse.statusCode
                     #if DEBUG
                         print(statusCode)
                     #endif
                 }
                 else {
-                        if (error != nil) {
-                            #if DEBUG
-                                print(error!.description)
-                            #endif
-                        }
+                    if (error != nil) {
+                        #if DEBUG
+                            print(error!.localizedDescription)
+                        #endif
+                    }
                 }
             }
             task.resume()
@@ -122,22 +119,22 @@ class GATracker {
     
     func screenView(screenName: String, customParameters: Dictionary<String, String>?) {
         /*
-            A screenview hit, use screenname
-        */
+         A screenview hit, use screenname
+         */
         var params = ["cd" : screenName]
         if (customParameters != nil) {
             for (key, value) in customParameters! {
                 params.updateValue(value, forKey: key)
             }
         }
-        self.send("screenview", params: params)
+        self.send(type: "screenview", params: params)
     }
     
     func event(category: String, action: String, label: String?, customParameters: Dictionary<String, String>?) {
         /*
-            An event hit with category, action, label
-        */
-         
+         An event hit with category, action, label
+         */
+        
         //event parameters category, action and label
         var params = ["ec" : category, "ea" : action, "el" : label ?? ""]
         if (customParameters != nil) {
@@ -145,13 +142,13 @@ class GATracker {
                 params.updateValue(value, forKey: key)
             }
         }
-        self.send("event", params: params)
+        self.send(type: "event", params: params)
     }
     
     func exception(description: String, isFatal:Bool, customParameters: Dictionary<String, String>?) {
         /*
-            An exception hit with exception description (exd) and "fatality"  (Crashed or not) (exf)
-        */
+         An exception hit with exception description (exd) and "fatality"  (Crashed or not) (exf)
+         */
         var fatal="0";
         if (isFatal){
             fatal = "1";
@@ -163,7 +160,7 @@ class GATracker {
                 params.updateValue(value, forKey: key)
             }
         }
-        self.send("exception", params: params)
+        self.send(type: "exception", params: params)
         
     }
 }
